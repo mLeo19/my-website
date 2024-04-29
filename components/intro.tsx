5f234fe5adbf13b3ddf77b1077256296038bb8ce
Leonardo <maicelo0110@gmail.com>
2024-04-28 20:24:22 -0400

    updated resume button

--- components/intro.tsx
+++ components/intro.tsx
@@ -73,10 +73,10 @@ export default function Intro() {
         </Link>
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/Resume.pdf"
           download
         >
-          Download CV{" "}
+          Download resume{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
         <a
