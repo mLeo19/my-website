5085f2cc4ed20ab9cca3ecf629780368a6ff65fc
Leonardo <maicelo0110@gmail.com>
2026-08-08 18:27:34 -0400

    Hide decorative blur backgrounds on mobile for performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
-        <div className="bg-[#e2fbe2] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#508357]"></div>
-        <div className="bg-[#d7f9fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#437e7e]"></div>
+        <div className="hidden sm:block bg-[#e2fbe2] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#508357]"></div>
+        <div className="hidden sm:block bg-[#d7f9fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#437e7e]"></div>
         <ThemeContextProvider>
         <ActiveSectionContextProvider>
             <Header />
